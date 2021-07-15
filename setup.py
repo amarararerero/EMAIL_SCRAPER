aaa16cc23f4a5774b50d53d6ec8d2fc045d290e2
Amar534 <80697941+amarararerero@users.noreply.github.com>
2021-07-15 14:43:09 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,7 @@
+import os
+
+os.system('sudo apt install wget')
+os.system('clear')
+os.system('pip3 install re bs4 requests')
+os.system('cd /usr/share/figlet')
+os.system('wget http://www.figlet.org/fonts/poison.flf')
